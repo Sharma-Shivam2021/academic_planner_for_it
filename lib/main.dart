0e0e2f1df0ffcfe7dc952690bfafac2ef7e7e01c
Shivam Sharma <2571sharmashivam@gmail.com>
2024-09-23 23:38:34 +0530

    Made app portrait up only

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 //Project Packages
 import 'package:academic_planner_for_it/utilities/theme/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 //Project Classes
@@ -9,6 +10,7 @@ import 'utilities/routes/routes.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(
     const ProviderScope(
       child: MyApp(),
